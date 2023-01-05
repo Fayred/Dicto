a7aa6a447f3fc162b68adba1dc46c7ac55b02e1d
Fayred <55796912+Fayred@users.noreply.github.com>
2023-01-05 12:00:14 +0100

    Update dicto.py

--- Python/modules/dicto.py
+++ Python/modules/dicto.py
@@ -20,49 +20,49 @@ class dicto(Tk):
         self.title(title)
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
-        self._create_menu_bar()
+        self.create_menu_bar()
         
         self._tab_control = Notebook(self)
         self._tab.append(text_tab(self._tab_control))
         self._tab_control.add(self._tab[self._number_of_tab-1], text="Untitled")
         self._tab_control.grid(sticky="nsew", column=0, row=0)
 
-        self._close_tab_button = Button(text="[×] Close current tab", command=self._close_current_tab)
+        self._close_tab_button = Button(text="[×] Close current tab", command=self.close_current_tab)
         self._close_tab_button.grid(sticky="w")
 
-        self._tab_control.bind("<<NotebookTabChanged>>", lambda func: self._tab_changed())
+        self._tab_control.bind("<<NotebookTabChanged>>", lambda func: self.tab_changed())
 
-    def _create_menu_bar(self):
+    def create_menu_bar(self):
         menu_bar = Menu(self)
         self.config(menu=menu_bar)
 
         menu_file = Menu(menu_bar, tearoff=0)
-        menu_file.add_command(label="New", command=self._new_file)
-        menu_file.add_command(label="Open", command=self._open_file)
-        menu_file.add_command(label="Save", command=self._save_file)
-        menu_file.add_command(label="Save as", command=self._saveAs_file)
+        menu_file.add_command(label="New", command=self.new_file)
+        menu_file.add_command(label="Open", command=self.open_file)
+        menu_file.add_command(label="Save", command=self.save_file)
+        menu_file.add_command(label="Save as", command=self.saveAs_file)
         menu_file.add_separator()
-        menu_file.add_command(label="Exit", command=self._quit_window)
+        menu_file.add_command(label="Exit", command=self.quit_window)
         menu_bar.add_cascade(label="File", menu=menu_file)
 
         menu_help = Menu(menu_bar, tearoff=0)
-        menu_help.add_command(label="About", command=self._about)
+        menu_help.add_command(label="About", command=self.about)
         menu_bar.add_cascade(label="Help", menu=menu_help)
 
-    def _close_current_tab(self):
+    def close_current_tab(self):
         if(self._number_of_tab > 1):
             del self._tab[self._current_tab]
             self._tab_control.forget(self._tab_control.select())
             self._number_of_tab -= 1
 
-    def _new_file(self):
+    def new_file(self):
         self._tab.append(text_tab(self._tab_control))
         self._tab_control.add(self._tab[self._number_of_tab], text="Untitled")
         self._tab_control.select(self._number_of_tab)
 
         self._number_of_tab += 1
 
-    def _open_file(self):
+    def open_file(self):
         path = askopenfilename()
         if len(path) != 0:
             try:
@@ -81,7 +81,7 @@ class dicto(Tk):
             except UnicodeDecodeError:
                 showerror(title="Error", message="That file can't be read")
 
-    def _save_file(self):
+    def save_file(self):
         if self._tab[self._current_tab].path == "":
             self._tab[self._current_tab].path = asksaveasfilename()
 
@@ -92,19 +92,19 @@ class dicto(Tk):
             self._tab_control.tab(self._tab[self._current_tab], text=basename(self._tab[self._current_tab].path))
             
             
-    def _saveAs_file(self):
+    def saveAs_file(self):
         path = asksaveasfilename()
         if len(path) != 0:
             file = open(path, "w")
             file.write(self._tab[self._current_tab].textEdit.get(1.0, END))
             file.close()
 
-    def _quit_window(self):
+    def quit_window(self):
         if askyesno(title="Exit", message="Are you sure than you want to quit ?"):
             self.destroy()
 
-    def _about(self):
+    def about(self):
         showinfo(title="About", message="Dicto v1.0\nCreator : Fayred\nBuilt on 28 dec. 2022")
 
-    def _tab_changed(self):
+    def tab_changed(self):
         self._current_tab = self._tab_control.index(self._tab_control.select())
